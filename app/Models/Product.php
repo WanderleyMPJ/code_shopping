abaead96aeaff9e9a568ae792c9b3b39c8473496
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-06-19 11:14:36 -0400

    Esclusão Logica

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,13 @@ namespace CodeShopping\Models;
 
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use Sluggable;
+    use Sluggable, SoftDeletes;
 
+    protected $dates = ['deleted_at'];
     protected $fillable = ['name', 'active', 'description', 'price'];
 
     public function sluggable(): array
