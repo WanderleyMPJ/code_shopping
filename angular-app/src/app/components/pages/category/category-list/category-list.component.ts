85b60eb66ba6c5b172c996e26e2a39332f848825
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-06-29 15:25:32 -0400

    Consumindo categorias no componente de listagem

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories = [];
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
+      this.http.get<Array<any>>('http://localhost:8000/api/categories', {
+          headers:{
+              'Authorization': `Bearer ${token}`
+          }
+      })
+          .subscribe(data => this.categories = data);
   }
 
 }
