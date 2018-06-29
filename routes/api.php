d8d3898bfe970e4c3863ccecf63bfb914c3ab160
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-06-29 16:08:21 -0400

    Estilizando componente de listagem de categorias

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,9 @@ Route::group(['namespace' => 'Api', 'as' => 'api.' ], function (){
     Route::name('login')->post('login', 'AuthController@login');
     Route::name('refresh')->post('refresh', 'AuthController@refresh');
 
-    Route::group(['middleware' => ['auth:api', 'jwt.refresh']], function(){
+    Route::group(['middleware' => ['auth:api',
+    //    'jwt.refresh'
+    ]], function(){
         Route::name('logout')->post('logout', 'AuthController@logout');
         Route::name('me')->get('me', 'AuthController@me');
         Route::patch('products/{product}/restore','ProductController@restore');
