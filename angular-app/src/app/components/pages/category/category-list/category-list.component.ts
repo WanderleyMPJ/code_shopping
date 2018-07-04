b90d68e585aa63b3df1c20aa97c02af88f054d7b
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-04 15:31:58 -0400

    Criando Cadastro de Categoria

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 
+declare let $;
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -9,22 +11,45 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CategoryListComponent implements OnInit {
 
+
+
   categories : Array<{id: number, name: string, active: boolean, created_at: {date: string}}> = [];
+
+  category = {
+      name: ''
+  }
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  submit(){
       const token = window.localStorage.getItem('token');
-     // this.http.get<{data: Array<any>}>('http://localhost:8000/api/categories', { *****exemplo
+    this.http.post('http://localhost:8000/api/categories', this.category, {
+        headers:{
+          'Authorization': `Bearer ${token}`
+        }
+      })
+        .subscribe((category) => {
+            console.log(category);
+            this.getCategories();
+            $('#exampleModal').modal('hide')
+        });
+  }
+
+  getCategories(){
+      const token = window.localStorage.getItem('token');
+      // this.http.get<{data: Array<any>}>('http://localhost:8000/api/categories', { *****exemplo
       this.http.get<{data: Array<{id: number, name: string, active: boolean, created_at: {date: string}}>}>
-        ('http://localhost:8000/api/categories', {
+      ('http://localhost:8000/api/categories', {
           headers:{
               'Authorization': `Bearer ${token}`
           }
       })
           .subscribe(response => this.categories = response.data);
   }
-
 }
