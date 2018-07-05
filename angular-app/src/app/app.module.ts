21394b7f7aa6a53db4f65a2db532689be10acf11
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-05 15:38:54 -0400

    Criando Modal Bootstrap componetizado

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AlertErrorComponent } from './components/bootstrap/alert-error/alert-er
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {RouterModule, Routes} from "@angular/router";
+import { ModalComponent } from './components/bootstrap/modal/modal.component';
 
 
 const routes: Routes = [
@@ -30,7 +31,8 @@ const routes: Routes = [
     AppComponent,
     LoginComponent,
     CategoryListComponent,
-    AlertErrorComponent
+    AlertErrorComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
