4040aa42f59669157a807c33980039f692f98562
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-05 11:38:21 -0400

    Tela Inicial
    Apontado tela inicial e redirecionamento tela em branco

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -15,6 +15,11 @@ const routes: Routes = [
     },
     {
         path: 'categories/list', component: CategoryListComponent
+    },
+    {
+        path: '',
+            redirectTo: '/login',
+            pathMatch: 'full'
     }
 ]
 
