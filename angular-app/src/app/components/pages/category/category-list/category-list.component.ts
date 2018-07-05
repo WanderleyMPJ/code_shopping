be8c2e755f96d32897948e9ccba54d6e7a3ee64c
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-05 15:33:55 -0400

    Criando Cadastro de Categoria

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+declare let $;
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -10,20 +12,42 @@ export class CategoryListComponent implements OnInit {
 
   categories : Array<{id: number, name: string, active: boolean, created_at: {date: string}}> = [];
 
+  category = {
+      name: ''
+  }
+
   constructor(private http: HttpClient) {
 
   }
 
-  ngOnInit() {
-      const token = window.localStorage.getItem('token');
-      // this.http.get<{data: Array<any>}>('http://localhost:8000/api/categories', { *****exemplo
-      this.http.get<{data: Array<{id: number, name: string, active: boolean, created_at: {date: string}}>}>
-      ('http://localhost:8000/api/categories', {
-          headers:{
-              'Authorization': `Bearer ${token}`
-          }
-      })
-          .subscribe(response => this.categories = response.data);
-  }
+    ngOnInit() {
+        this.getCategories();
+    }
+
+    submit(){
+        const token = window.localStorage.getItem('token');
+        this.http.post('http://localhost:8000/api/categories', this.category, {
+            headers:{
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .subscribe((category) => {
+                console.log(category);
+                this.getCategories();
+                $('#exampleModal').modal('hide')
+            });
+    }
+
+    getCategories(){
+        const token = window.localStorage.getItem('token');
+        // this.http.get<{data: Array<any>}>('http://localhost:8000/api/categories', { *****exemplo
+        this.http.get<{data: Array<{id: number, name: string, active: boolean, created_at: {date: string}}>}>
+        ('http://localhost:8000/api/categories', {
+            headers:{
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .subscribe(response => this.categories = response.data);
+    }
 
 }
