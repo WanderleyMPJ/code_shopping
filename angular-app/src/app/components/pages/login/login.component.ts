f8856f382eb1e8ca27f4268c4d37b728bad5c401
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-05 11:36:15 -0400

    Single Page
    criado componente category e rotas, não foi apontado ainda a tela inicial

--- angular-app/src/app/components/pages/login/login.component.ts
+++ angular-app/src/app/components/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'login',
@@ -13,7 +14,9 @@ export class LoginComponent implements OnInit {
         password: 'secret'
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) {
+
+  }
 
   ngOnInit() {
   }
@@ -22,13 +25,14 @@ export class LoginComponent implements OnInit {
         //generics - Java
         this.http.post<any>('http://localhost:8000/api/login', this.credentials)
             .subscribe((data) => {
+                this.router.navigate(['categories/list']);
                 const token = data.token;
-                this.http.get('http://localhost:8000/api/categories', {
-                    headers:{
-                        'Authorization': `Bearer ${token}`
-                    }
-                })
-                    .subscribe(data => console.log(data));
+                // this.http.get('http://localhost:8000/api/categories', {
+                //     headers:{
+                //         'Authorization': `Bearer ${token}`
+                //     }
+                // })
+                //     .subscribe(data => console.log(data));
             });
         return false;
     }
