a51c3d0baf8f9ee202dfe08af9967c54fb8cb36e
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-05 09:11:56 -0400

    Login
    Criando o component login e direcionando a pagina para tal.

---
+++ angular-app/src/app/components/pages/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  credentials = {
+    email: 'admin@user.com',
+      password: 'secret'
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  submit(){
+        //generics - Java
+        this.http.post<any>('http://localhost:8000/api/login', this.credentials)
+            .subscribe((data) => {
+                const token = data.token;
+                this.http.get('http://localhost:8000/api/categories', {
+                    headers:{
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                    .subscribe(data => console.log(data));
+            });
+        return false;
+  }
+
+}
