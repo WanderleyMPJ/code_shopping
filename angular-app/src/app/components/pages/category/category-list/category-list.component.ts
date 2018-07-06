2411a50823a95aeaffd485823ecc012113fb2bea
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-06 06:07:55 -0400

    Usando novo modal no cadastro de categorias
    Testado e funcionando tudo no sistema até aqui.

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -36,8 +36,8 @@ export class CategoryListComponent implements OnInit {
         })
             .subscribe((category) => {
                 console.log(category);
+                this.modal.hide();
                 this.getCategories();
-                $('#exampleModal').modal('hide')
             });
     }
 
