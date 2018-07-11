2a31a54ea11e15d31acb790fc61d2fb375f3861e
Wanderley Macedo <wanderley.efficax@gmail.com>
2018-07-11 16:51:11 -0400

    Criando campo select para listar categorias

--- angular-app/src/app/components/pages/product-category/product-category-list/product-category-list.component.ts
+++ angular-app/src/app/components/pages/product-category/product-category-list/product-category-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ProductHttpService} from "../../../../services/http/product-http.service";
-import {Product, ProductCategory} from "../../../../Models";
+import {Category, Product, ProductCategory} from "../../../../Models";
 import {ProductCategoryHttpService} from "../../../../services/http/product-category-http.service";
+import {CategoryHttpService} from "../../../../services/http/category-http.service";
 
 @Component({
   selector: 'app-product-category-list',
@@ -14,17 +15,38 @@ export class ProductCategoryListComponent implements OnInit {
   productid: number;
   product: Product = null;
   productCategory: ProductCategory = null;
+  categories: Category[] = [];
+  categoriesid: number[] = [];
 
   constructor(private route: ActivatedRoute,
               private productHttp: ProductHttpService,
-              private productCategoryHttp: ProductCategoryHttpService) { }
+              private productCategoryHttp: ProductCategoryHttpService,
+              private categoryHttp: CategoryHttpService) {
+
+  }
+
+
 
   ngOnInit() {
     this.route.params.subscribe(params => {
+      this.getCategories();
       this.productid = params.product;
       this.getProduct();
       this.getProductCategory();
     });
+  }
+
+  change(){
+    console.log(this.categoriesid);
+  }
+
+  getCategories(){
+      this.categoryHttp.list(1)
+          .subscribe(response => {
+              this.categories = response.data;
+          })
+
+
   }
 
   getProduct(){
