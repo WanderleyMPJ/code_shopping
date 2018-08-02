814e805aa4fcbf29a078152510296e246e350fa6
Wanderley Macedo Pinheiro <wanderley.efficax@gmail.com>
2018-08-01 22:02:58 -0400

    Criando interceptor para renovar token

---
+++ angular-app/src/app/services/refresh-token-interceptor.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RefreshTokenInterceptorService implements HttpInterceptor{
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any>{
+    return next
+        .handle(req)
+        .pipe(
+          tap( (event: HttpEvent<any>) => {
+            console.log(event);
+            this.setNewTokenIfResponseValid(event)
+          })
+        )
+  }
+
+  private setNewTokenIfResponseValid(event: HttpEvent<any>){
+    if(event instanceof HttpResponse){
+      const authorizationHeader = event.headers.get('authorization');
+      console.log(authorizationHeader)
+    }
+}
+}
