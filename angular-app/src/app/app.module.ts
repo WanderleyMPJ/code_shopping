e32c2893d152e1767d618089326e0b9779ca6c09
Wanderley Macedo Pinheiro <wanderley.efficax@gmail.com>
2018-08-21 17:57:49 -0400

    Modificando diretiva isInvalid

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -33,7 +33,7 @@ import { SortColumnComponent } from './components/common/sort-column/sort-column
 import { CategorySearchFormComponent } from './components/pages/category/category-search-form/category-search-form.component';
 import { CategoryFormComponent } from './components/pages/category/category-form/category-form.component';
 import { FieldErrorComponent } from './components/bootstrap/field-error/field-error.component';
-import { IsInvalidDirective } from './directives/is-invalid.directive';
+import {IsInvalidControlDirective, IsInvalidDirective} from './directives/is-invalid.directive';
 import { ListErrorComponent } from './components/bootstrap/list-error/list-error.component';
 import { CardErrorComponent } from './components/bootstrap/card-error/card-error.component';
 import { ProductInputListComponent } from './components/pages/product-input/product-input-list/product-input-list.component';
@@ -82,6 +82,7 @@ import {Select2Module} from "ng2-select2";
     CategoryFormComponent,
     FieldErrorComponent,
     IsInvalidDirective,
+    IsInvalidControlDirective,
     ListErrorComponent,
     CardErrorComponent,
     ProductInputListComponent,
