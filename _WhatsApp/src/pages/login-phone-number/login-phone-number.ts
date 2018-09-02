3701d89b2d022efff4ff2c60fc9e38a722acc63f
Wanderley <wanderley.efficax@gmail.com>
2018-09-02 12:03:26 -0400

    Traduzindo FirebaseUI
    Habilitar no firebase a autenticação por telefone.
    
    -npm install scriptjs@2.5.8 --save

--- _WhatsApp/src/pages/login-phone-number/login-phone-number.ts
+++ _WhatsApp/src/pages/login-phone-number/login-phone-number.ts
@@ -3,6 +3,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase';
 import * as firebaseui from 'firebaseui';
 import firebaseConfig from '../../app/firebase-config';
+import scriptjs from 'scriptjs';
+
+declare const firebraseui;
+(<any>window).firebase = firebase;
 
 @IonicPage()
 @Component({
@@ -15,14 +19,16 @@ export class LoginPhoneNumberPage {
   }
 
   ionViewDidLoad() {
-    firebase.initializeApp(firebaseConfig);
-    const uiConfig = {
-      signInOptions: [
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
-      ]
-    }
-    const ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebase-ui',uiConfig);
+    scriptjs('https://www.gstatic.com/firebasejs/ui/3.4.0/firebase-ui-auth__pt.js', () => {
+        firebase.initializeApp(firebaseConfig);
+        const uiConfig = {
+            signInOptions: [
+                firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            ]
+        }
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
+        ui.start('#firebase-ui',uiConfig);
+    });
   }
 
 }
