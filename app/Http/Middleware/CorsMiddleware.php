13ddcd3b5c4c6b30addd42069dd46119acd8f01b
Wanderley <wanderley.efficax@gmail.com>
2018-09-05 22:49:42 -0400

    Melhoria nos endereços permitidos do CORS

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -6,10 +6,22 @@ use Closure;
 
 class CorsMiddleware
 {
+    private $origins = [
+        'http://localhost:4200',
+        'http://localhost:8100',
+    ];
+
     public function handle($request, Closure $next)
     {
+        $requestOrigen = $request->headers->get('Origin');
+        if (in_array($requestOrigen, $this->origins)){
+            $allowOrigin = $requestOrigen;
+        }
+
         if($request->is('api/*')){
-            header('Access-Control-Allow-Origin: http://localhost:4200');
+            if(isset($allowOrigin)){
+                header("Access-Control-Allow-Origin: $allowOrigin");
+            }
             header('Access-Control-Allow-Headers: Content-Type,Authorization');
             header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
             header('Access-Control-Expose-Headers: Authorization');
