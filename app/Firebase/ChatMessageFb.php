d8745839ba9947ce6437df30be9341cc4f3e8046
Wanderley <wanderley.efficax@gmail.com>
2018-10-06 11:01:56 -0400

    Gerando mensagens falsas

--- app/Firebase/ChatMessageFb.php
+++ app/Firebase/ChatMessageFb.php
@@ -9,19 +9,39 @@
 namespace CodeShopping\Firebase;
 
 
+use CodeShopping\Models\ChatGroup;
+
 class ChatMessageFb
 {
     use FirebaseSync;
 
+    private $chatGroup;
+
     public function create(array $data)
     {
-        $type = $data['tipo'];
+        $this->chatGroup = $data['chat_group'];
+        $type = $data['type'];
         switch ($type){
             case 'audio':
             case 'image':
-            case 'text' :
         }
 
-        
+        $reference = $this->getMessagesReference();
+        $reference->push([
+            'type' => $data['type'],
+            'content' => $data['content'],
+            'created_at' => ['.sv' => 'timestamp'],
+            'user_id' => $data['firebase_uid']
+        ]);
+    }
+
+    public function deleteMessages(ChatGroup $chatGroup){
+        $this->chatGroup = $chatGroup;
+        $this->getMessagesReference()->remove();
+    }
+
+    private function getMessagesReference(){
+        $path = "/chat_groups/{$this->chatGroup->id}/messages";
+        return $this->getFirebaseDatabase()->getReference($path);
     }
 }
